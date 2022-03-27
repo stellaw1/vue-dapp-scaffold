134e9e53badf5e8ca542d2081fae91dbfad2ea34
stellaw1 <stella01wang@gmail.com>
2022-03-27 00:02:28 -0700

    working on storing state for SOL balance

--- app/src/main.js
+++ app/src/main.js
@@ -10,7 +10,25 @@ const router = createRouter({
     routes,
 })
 
+// // Create a new store instance.
+// import { createStore } from 'vuex'
+
+// const UserSOLBalanceStore = createStore({
+//   state () {
+//     return {
+//       balance: 0
+//     }
+//   },
+//   mutations: {
+//     increment (state) {
+//       state.count++
+//     }
+//   }
+// })
+
+
 // Create App
 import { createApp } from 'vue'
 import App from './App.vue'
 createApp(App).use(router).mount('#app')
+// .use(UserSOLBalanceStore)
