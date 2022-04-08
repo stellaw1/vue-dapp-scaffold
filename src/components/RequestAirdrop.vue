e829a157f8e8c0944d20734d94bddcec1d9885c9
Stella Wang <46392676+stellaw1@users.noreply.github.com>
2022-04-08 02:11:22 -0700

    Brian's review changes (#4)
    * refactor + comments for airdrop
    
    * fixes Brian's review changes
    
    Co-authored-by: brian ruddy <brian@ruddy.io>

--- src/components/RequestAirdrop.vue
+++ src/components/RequestAirdrop.vue
@@ -1,41 +1,39 @@
 <script setup>
-import store from '../vuex'
+import store from '../vuex';
 import { useWallet } from 'solana-wallets-vue'
 import { Connection, clusterApiUrl, LAMPORTS_PER_SOL } from '@solana/web3.js';
 
-const { publicKey, connected, sendTransaction } = useWallet();
+const { publicKey, connected } = useWallet();
 
-const onClick = async () => {
-        if (!publicKey) {
-            console.log('error', `Wallet not connected!`);
-            return;
-        }
+function setBalance (balance) {
+    store.commit('updateBalance', { balance });
+}
 
-        let signature = '';
-        try {
-            const connection = new Connection(clusterApiUrl('devnet'));
-
-            signature = await connection.requestAirdrop(publicKey.value, LAMPORTS_PER_SOL);
-            await connection.confirmTransaction(signature, 'confirmed');
-            let balance = await connection.getBalance(
-                publicKey.value,
-                'confirmed'
-            );
-            balance = balance / LAMPORTS_PER_SOL;
-            store.commit("updateBalance", {balance: balance})
-            console.log("success, balance is now", store.state.userSOLBalance);
-
-        } catch (error) {
-            console.log('error', `Airdrop failed! ${error?.message}`, signature);
-        }
+async function requestAirdrop () {
+    if (!publicKey) {
+        console.error('[requestAirdrop] public key not found');
+        return;
     }
+
+    const connection = new Connection(clusterApiUrl('devnet'));
+    const signature = await connection.requestAirdrop(publicKey.value, LAMPORTS_PER_SOL);
+
+    await connection.confirmTransaction(signature, 'confirmed');
+
+    const lamports = await connection.getBalance(publicKey.value, 'confirmed');
+    const balance = lamports / LAMPORTS_PER_SOL;
+
+    setBalance(balance);
+
+    console.log(`[requestAirdrop] success, balance is now ${balance} sol`);
+}
 </script>
 
 <template>
     <div>
         <button
             class="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
-            @click="onClick"
+            @click="requestAirdrop"
         >
             <span>
                 Airdrop 1
